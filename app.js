37b7d5128eeee51443c27a9f6fc101250d6e843c
Joseph M <jmedina1985@hotmail.com>
2020-08-20 12:38:12 -0700

    Prepped for logic

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 class LittleDrumMachine {
   constructor() {
+    this.pad = document.querySelectorAll(".pad");
+    this.playButton = document.querySelector(".play");
     this.kickAudio = document.querySelector(".kick-sample");
     this.snareAudio = document.querySelector(".snare-sample");
     this.clapAudio = document.querySelector(".clap-sample");
@@ -8,12 +10,29 @@ class LittleDrumMachine {
     this.ohhAudio = document.querySelector(".ohh-sample");
     this.bassAudio = document.querySelector(".bass-sample");
     this.synthAudio = document.querySelector(".synth-sample");
+    this.index = 0;
     this.bpm = 120;
     this.isPlaying = null;
+    this.selects = document.querySelectorAll("select");
+    this.muteButtons = document.querySelectorAll(".mute");
+    this.tempoSlider = document.querySelector(".tempo-slider");
   }
+  activatedPad() {
+    this.classList.toggle("activated");
+  }
+  repeat() {}
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+  }
+  changeSample() {}
+  mute() {}
+  changeTempo() {}
+  updateTempo() {}
 }
 
-// Init
 const littleDrumMachine = new LittleDrumMachine();
 
 // Event Listeners
+littleDrumMachine.playButton.addEventListener("click", function () {
+  littleDrumMachine.start();
+});
