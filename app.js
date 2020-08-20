db467d4f62b358e27679445fc4437a0dfaf261e3
Joseph M <jmedina1985@hotmail.com>
2020-08-20 13:44:14 -0700

    Some major changes with logic

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 class LittleDrumMachine {
   constructor() {
-    this.pad = document.querySelectorAll(".pad");
-    this.playButton = document.querySelector(".play");
+    this.pads = document.querySelectorAll(".pad");
+    this.playButton = document.querySelector(".play-button");
     this.kickAudio = document.querySelector(".kick-sample");
     this.snareAudio = document.querySelector(".snare-sample");
     this.clapAudio = document.querySelector(".clap-sample");
@@ -20,9 +20,63 @@ class LittleDrumMachine {
   activatedPad() {
     this.classList.toggle("activated");
   }
-  repeat() {}
+  repeat() {
+    let step = this.index % 16;
+    const activeBeat = document.querySelectorAll(`.beat${step}`);
+    // Looping over the pads
+    activeBeat.forEach((beat) => {
+      beat.style.animation = `playHead 0.3s alternate ease-in-out 2`;
+      if (beat.classList.contains("activated")) {
+        if (beat.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (beat.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (beat.classList.contains("clap-pad")) {
+          this.clapAudio.currentTime = 0;
+          this.clapAudio.play();
+        }
+        if (beat.classList.contains("tom-pad")) {
+          this.tomAudio.currentTime = 0;
+          this.tomAudio.play();
+        }
+        if (beat.classList.contains("chh-pad")) {
+          this.chhAudio.currentTime = 0;
+          this.chhAudio.play();
+        }
+        if (beat.classList.contains("ohh-pad")) {
+          this.ohhAudio.currentTime = 0;
+          this.ohhAudio.play();
+        }
+        if (beat.classList.contains("bass-pad")) {
+          this.bassAudio.currentTime = 0;
+          this.bassAudio.play();
+        }
+        if (beat.classList.contains("synth-pad")) {
+          this.synthAudio.currentTime = 0;
+          this.synthAudio.play();
+        }
+      }
+    });
+    this.index++;
+  }
   start() {
-    const interval = (60 / this.bpm) * 1000;
+    const interval = (30 / this.bpm) * 1000;
+    if (this.isPlaying) {
+      clearInterval(this.isPlaying);
+      this.playButton.innerHTML = `<i class="fas fa-play"></i>`;
+      this.playButton.classList.remove("activated");
+      this.isPlaying = null;
+    } else {
+      this.playButton.innerHTML = `<i class="fas fa-pause"></i>`;
+      this.playButton.classList.add("activated");
+      this.isPlaying = setInterval(() => {
+        this.repeat();
+      }, interval);
+    }
   }
   changeSample() {}
   mute() {}
@@ -33,6 +87,10 @@ class LittleDrumMachine {
 const littleDrumMachine = new LittleDrumMachine();
 
 // Event Listeners
+littleDrumMachine.pads.forEach((pad) => {
+  pad.addEventListener("click", littleDrumMachine.activatedPad);
+});
+
 littleDrumMachine.playButton.addEventListener("click", function () {
   littleDrumMachine.start();
 });
